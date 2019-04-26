b569fa64e5985737c63350102107e2ae89962bbf
Hiroshi Iwata <iwaiwa2mail@gmail.com>
2019-04-26 11:37:14 +0900

    謎の再起動バグを修正した

--- tester_74xx_i2c/tester_74xx_i2c.ino
+++ tester_74xx_i2c/tester_74xx_i2c.ino
@@ -14,16 +14,17 @@
 //LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 LiquidCrystal lcd(4, 5, 6, 7, 8, 9);
 
-#define NO_TYPES 7
+#define NO_TYPES 9
 char* ic_type[] = {
   "IV1 (74LS04)", // 1-input NOT
   "AN2 (74LS08)", // 2-input AND
   "OR2 (74LS32)", // 2-input OR
   "ND2 (74LS00)", // 2-input NAND
+  "ND3 (74LS10)", // 3-input NAND
   "NR2 (74LS02)", // 2-input NOR
   "EO2 (74LS86)", // 2-input XOR
-  "DFF (74LS74)"  // D flip-flop
-  // JK flip-flopは16pinだからテストできねぇｗ
+  "DFF (74LS74)", // D flip-flop
+  "JKF (74LS107)" // JK flip-flop
 };
 
 /*
@@ -46,28 +47,34 @@ char* ic_type[] = {
   Name   |  A7  A6  A5  A4  A3  A2  A1  A0  B7  B6  B5  B4  B3  B2  B1  B0
   74pin  |  13  12  11  10   9   8                   6   5   4   3   2   1
   ND2_DIR| out out  in out out  in                  in out out  in out out
+  ND3_DIR| out  in out out out  in                  in out out out out out
   NR2_DIR|  in out out  in out out                 out out  in out out  in
   IV1_DIR| out  in out  in out  in                  in out  in out  in out
   DFF_DIR| out out out out  in  in                  in  in out out out out
+  JKF_DIR| out out out out out out                  in  in out  in  in out
 */
 // bit number        5432109876543210
 #define ND2_IO_DIR 0b0010011111100100 // I/O Direction 0:output 1:input
+#define ND3_IO_DIR 0b0100011111100000 // I/O Direction 0:output 1:input
 #define NR2_IO_DIR 0b1001001111001001 // I/O Direction 0:output 1:input
 #define IV1_IO_DIR 0b0101011111101010 // I/O Direction 0:output 1:input
 #define DFF_IO_DIR 0b0000111111110000 // I/O Direction 0:output 1:input
+#define JKF_IO_DIR 0b0000001111110110 // I/O Direction 0:output 1:input
 
 volatile int mode = -1;
 volatile bool flag_test = false;
 
 int lcd_mode = -1;
 int pt, ct;
+char* selected_ic = NULL;
 
 void setup() {
+  selected_ic = (char*)malloc(sizeof(char) * 5);
   lcd.begin(16, 2);  // set columns and rows of LCD
   lcd.setCursor(0, 0);
   lcd.print("Tester for 74xx"); // max 16 character
   lcd.setCursor(0, 1);
-  lcd.print("    version 1.1 ");
+  lcd.print("    version 1.5 ");
 
   // set switch direction
   pinMode(SW_IC_SELECT, INPUT_PULLUP);
@@ -115,10 +122,9 @@ void loop() {
 
 void exec_test() {
   unsigned int response = 0x0000; // 0: OK / 1: NG
-  char selected_ic = (char*)malloc(sizeof(char) * 4);
   Serial.print("Test start -> ");
   //  mode = 6;
-  selected_ic = get_ic_name3(selected_ic, mode);
+  get_ic_name3(mode);
   if ( strcmp(selected_ic, "IV1") == 0 ) {
     Serial.println("NOT");
     response |= apply_to_1input1(0, 1);
@@ -141,6 +147,16 @@ void exec_test() {
     response |= apply_to_2input1(0, 1, 1);
     response |= apply_to_2input1(1, 0, 1);
     response |= apply_to_2input1(1, 1, 0);
+  } else if ( strcmp(selected_ic, "ND3") == 0 ) {
+    Serial.println("NAND3");
+    response |= apply_to_3input1(0, 0, 0, 1);
+    response |= apply_to_3input1(0, 0, 1, 1);
+    response |= apply_to_3input1(0, 1, 0, 1);
+    response |= apply_to_3input1(0, 1, 1, 1);
+    response |= apply_to_3input1(1, 0, 0, 1);
+    response |= apply_to_3input1(1, 0, 1, 1);
+    response |= apply_to_3input1(1, 1, 0, 1);
+    response |= apply_to_3input1(1, 1, 1, 0);
   } else if ( strcmp(selected_ic, "EO2") == 0 ) {
     Serial.println("XOR");
     response |= apply_to_2input1(0, 0, 0);
@@ -168,6 +184,21 @@ void exec_test() {
     response |= apply_to_4input1(1, 1, 1, 1, 1, 0); // positive edge triger D=1->1
     response |= apply_to_4input1(1, 1, 1, 0, 1, 0); // hold
     response |= apply_to_4input1(1, 0, 1, 0, 0, 1); // reset
+  } else if ( strcmp(selected_ic, "JKF") == 0 ) {
+    Serial.println("JKF");   // ck re j  k  q  qn
+    response |= apply_to_4input2(1, 0, 1, 0, 0, 1); // reset
+    response |= apply_to_4input2(1, 1, 0, 0, 0, 1); // J=K=0 Q=0 Preserve
+    response |= apply_to_4input2(1, 1, 1, 0, 0, 1); // J=1 K=0 Q=0->1
+    response |= apply_to_4input2(0, 1, 1, 0, 1, 0); // negative edge triger Q=0->1
+    response |= apply_to_4input2(1, 1, 0, 0, 1, 0); // J=K=0 Q=1 Preserve
+    response |= apply_to_4input2(0, 1, 0, 0, 1, 0); // negative edge triger Q=0->0
+    response |= apply_to_4input2(1, 1, 1, 1, 1, 0); // J=K=1 Toggle Q=1->0
+    response |= apply_to_4input2(0, 1, 1, 1, 0, 1); // negative edge triger Q=1->0
+    response |= apply_to_4input2(1, 1, 1, 1, 0, 1); // J=K=1 Toggle Q=0->1
+    response |= apply_to_4input2(0, 1, 1, 1, 1, 0); // negative edge triger Q=1->0
+    response |= apply_to_4input2(1, 1, 0, 1, 1, 0); // J=0 K=1 Q=1->0
+    response |= apply_to_4input2(0, 1, 0, 1, 0, 1); // negative edge triger Q=1->0
+    response |= apply_to_4input2(1, 0, 0, 0, 0, 1); // reset
   } else {
     response = 0xffff;
   }
@@ -186,6 +217,14 @@ unsigned int apply_to_2input1( byte test_a, byte test_b, byte expect ) {
   unsigned int b = (test_b << 14) | (test_b << 11) | (test_b << 3) | (test_b << 0); // 12pin  9pin 4pin 1pin (74LS00)
   unsigned int e = (expect << 13) | (expect << 10) | (expect << 5) | (expect << 2); // 11pin  8pin 6pin 3pin (74LS00)
   return apply_test( (~ND2_IO_DIR & (a | b)), e, ND2_IO_DIR );
+}
+// ND3
+unsigned int apply_to_3input1( byte test_a, byte test_b, byte test_c, byte expect ) {
+  unsigned int a = (test_a << 12) | (test_a <<  2) | (test_a << 0); // 10pin 3pin 1pin (74LS10)
+  unsigned int b = (test_b << 13) | (test_b <<  3) | (test_b << 1); // 11pin 4pin 2pin (74LS10)
+  unsigned int c = (test_c << 15) | (test_c << 11) | (test_c << 4); // 13pin 9pin 5pin (74LS10)
+  unsigned int e = (expect << 14) | (expect << 10) | (expect << 5); // 12pin 8pin 6pin (74LS10)
+  return apply_test( (~ND3_IO_DIR & (a | b | c)), e, ND3_IO_DIR );
 }
 // NR2
 unsigned int apply_to_2input2( byte test_a, byte test_b, byte expect ) {
@@ -210,6 +249,16 @@ unsigned int apply_to_4input1( byte test_clk, byte test_clr, byte test_pr, byte 
   unsigned int f = (exp_qn   << 10) | (exp_qn   << 5); //  8pin 6pin (74LS74)
   return apply_test( (~DFF_IO_DIR & (a | b | c | d)), (e | f), DFF_IO_DIR );
 }
+// JKFF (74LS107)
+unsigned int apply_to_4input2( byte test_clk, byte test_clr, byte test_j, byte test_k, byte exp_q, byte exp_qn ) {
+  unsigned int a = (test_clk << 14) | (test_clk << 11); // 12pin  9pin (74LS107)
+  unsigned int b = (test_clr << 15) | (test_clr << 12); // 13pin 10pin (74LS107)
+  unsigned int c = (test_j   << 10) | (test_j   <<  0); //  8pin  1pin (74LS107)
+  unsigned int d = (test_k   << 13) | (test_k   <<  3); // 11pin 4pin (74LS107)
+  unsigned int e = (exp_q    <<  4) | (exp_q    <<  2); //  5pin 3pin (74LS107)
+  unsigned int f = (exp_qn   <<  5) | (exp_qn   <<  1); //  6pin 2pin (74LS107)
+  return apply_test( (~JKF_IO_DIR & (a | b | c | d)), (e | f), JKF_IO_DIR );
+}
 unsigned int apply_test( unsigned test, unsigned e, unsigned io_dir ) {
   write_I2C_port(0x00, byte(io_dir >> 8)); // I/O Direction A 0:output 1:input
   write_I2C_port(0x01, byte(io_dir));      // I/O Direction B 0:output 1:input
@@ -219,12 +268,11 @@ unsigned int apply_test( unsigned test, unsigned e, unsigned io_dir ) {
   unsigned int data = read_I2C_port();
   unsigned int response = data & io_dir & 0b1111110000111111; // 出力応答だけ取り出す
   unsigned int result = response ^ e;        // 期待値とのxorをとって誤ってたら1で残る
-  /*
-    Serial.println("");
-    serial_print_bin("Test:", test);
-    serial_print_bin("Exp: ", e);
-    serial_print_bin("Rspn:", response);
-  */
+  Serial.println("");
+  serial_print_bin("Test:", test);
+  serial_print_bin("Exp: ", e);
+  serial_print_bin("Rspn:", response);
+
   //  serial_print_bin("IODI:", io_dir);
   //  serial_print_bin("Data:", data);
   //  serial_print_bin("Rslt:", result);
@@ -276,13 +324,15 @@ void lcd_print_result( char* result, unsigned int response ) {
   lcd.print(str);
 }
 
-char* get_ic_name3( char* str_type, int type ) {
+
+char* get_ic_name3( int type ) {
   //  strncpy( str_type, ic_type[type], 3 );
-  str_type[0] = ic_type[type][0];
-  str_type[1] = ic_type[type][1];
-  str_type[2] = ic_type[type][2];
-  str_type[3] = '\0';
-  return str_type;
+  selected_ic[0] = ic_type[type][0];
+  selected_ic[1] = ic_type[type][1];
+  selected_ic[2] = ic_type[type][2];
+  selected_ic[3] = '\0';
+  Serial.println(selected_ic);
+  return selected_ic;
 }
 
 // I2C I/O expander function
@@ -317,5 +367,3 @@ void change_mode() {
 void start_test() {
   flag_test = true;
 }
-
-
